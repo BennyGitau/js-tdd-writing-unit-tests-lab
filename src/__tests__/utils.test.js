f8eacd2e6022e920be2cb4c6774e6271e5612402
BensonGitau <bensonkamaugitau@gmail.com>
2024-05-03 15:48:09 +0300

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,28 @@
-// Your tests here
+import {isPalindrome} from '../utils'
+
+describe("isPalindrome", ()=>{
+    it('returns true if a word is palindrome', ()=>{
+        expect(isPalindrome('racecar')).toBe(true);
+    });
+
+    it('returns false in a word is not palindrome', ()=>{
+        expect(isPalindrome('car')).toBe(false);
+    });
+
+    it('returns false for in empty string', ()=>{
+        expect(isPalindrome("")).toBe(false)
+    });
+
+    it("throws an error if word has non-alphabetic characters",()=>{
+        expect(()=>{
+            isPalindrome("1word")
+        }).toThrow();
+    });
+    it("throws an error if word is not a string",()=>{
+        expect(()=>{
+            isPalindrome(23345)
+        }).toThrow();
+    });
+    
+
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,20 @@
 // Your code here
+export function isPalindrome(word){
+    if(word === "") {
+        return false
+    }
+    if(typeof word !== 'string'){
+        throw new Error ('not a string')
+    }
+
+    if(!word.match(/^[A-Za-z]+$/)){
+        throw new Error ('Enter letters only')
+    }
+
+    const reversedWord = word.toLowerCase().split('').reverse().join('');
+    console.log(reversedWord)
+    return reversedWord === word
+
+
+}
+isPalindrome('rAcECar')
